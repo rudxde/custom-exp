2a74e616be427609bd9c87cb9e36598d3115f826
rudxde <kj4derechte@gmail.com>
2020-02-10 10:37:50 +0100

    add simple documentation

--- src/functionality.ts
+++ src/functionality.ts
@@ -3,7 +3,16 @@ import { Parameter } from './syntax-tree/parameter';
 import { addDefaults } from './syntax-tree/default-functions';
 import { Operators } from './syntax-tree/operators';
 type FieldType = {
+    /**
+     * Name of the field. The name is without the '$' prefix. The prefix ('$') needs to be added in the expression, to select the field.
+     *
+     * @type {string}
+     */
     name: string;
+    /**
+     * Will be called, to evaluate the fields value for each mentioning of the field
+     *
+     */
     eval: () => IEvalResult;
 };
 
@@ -15,11 +24,32 @@ type OperationType = {
 };
 
 type FunctionType = {
+    /**
+     * Name of th function. Can be later accessed on other expressions with ```.<name>(<params...>)```
+     *
+     * @type {string}
+     */
     name: string;
+    /**
+     * type on which the function should be assigned to.
+     *
+     * @type {string}
+     */
     scopeType: string;
+    /**
+     * evaluates the function. 
+      * @param {any} scopeResult the result of the scope expression, where the function has ben called on.
+      * @param {Parameter[]} parameters all parameters, passed to the function.
+     */
     eval: (scopeResult: any, parameters: Parameter[]) => IEvalResult;
 };
 
+/**
+ * Defines a set of functionality for the custom expressions.
+ *
+ * @export
+ * @class Functionality
+ */
 export class Functionality {
     public fields: FieldType[] = [];
     public operations: OperationType[] = [];
@@ -32,6 +62,13 @@ export class Functionality {
             addDefaults(this);
         }
     }
+
+    /**
+     * adds a field to the functionality.
+     *
+     * @param {FieldType} a
+     * @memberof Functionality
+     */
     addField(a: FieldType): void {
         const existingIndex = this.fields.findIndex(x => x.name === a.name);
         if (existingIndex !== -1) {
@@ -40,6 +77,13 @@ export class Functionality {
             this.fields.push(a);
         }
     }
+
+    /**
+     * adds a function to the functionality.
+     *
+     * @param {FunctionType} a
+     * @memberof Functionality
+     */
     addFunctions(a: FunctionType): void {
         const existingIndex = this.functions.findIndex(x => x.name === a.name && x.scopeType === a.scopeType);
         if (existingIndex !== -1) {
@@ -48,6 +92,13 @@ export class Functionality {
             this.functions.push(a);
         }
     }
+
+    /**
+     * adds a operation to the functionality.
+     *
+     * @param {OperationType} a
+     * @memberof Functionality
+     */
     addOperation(a: OperationType): void {
         const existingIndex = this.operations.findIndex(x =>
             x.operator === a.operator
@@ -61,18 +112,42 @@ export class Functionality {
         }
     }
 
+    /**
+     * finds a field with the given name
+     *
+     * @param {string} name
+     * @returns {FieldType}
+     * @memberof Functionality
+     */
     getField(name: string): FieldType {
         const result = this.fields.find(x => x.name === name);
         if (!result) throw new Error(`No Field with identifier '${name}' found.`);
         return result;
     }
 
+    /**
+     * finds a function with the given name and matching type.
+     *
+     * @param {string} name
+     * @param {string} type
+     * @returns {FunctionType}
+     * @memberof Functionality
+     */
     getFunction(name: string, type: string): FunctionType {
         const result = this.functions.find(x => x.name === name && x.scopeType === type);
         if (!result) throw new Error(`Function ${name} not found for type ${type}`);
         return result;
     }
 
+    /**
+     * finds the operation with the mating types and operator.
+     *
+     * @param {string} leftType
+     * @param {string} rightType
+     * @param {Operators} operator
+     * @returns {OperationType}
+     * @memberof Functionality
+     */
     getOperation(leftType: string, rightType: string, operator: Operators): OperationType {
         const result = this.operations.find(x => x.leftType === leftType && x.rightType === rightType && x.operator === operator);
         if (!result) throw new Error(`No function found for operator '${operator}' for types '${leftType}'X'${rightType}'`);
--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,28 @@ import './syntax-tree/default-functions';
 import { IEvalResult } from './syntax-tree/expression';
 import { Functionality } from './functionality';
 
+/**
+ * Evaluates an custom expression with a given functionality set.
+ *
+ * @export
+ * @param {Functionality} functionality
+ * @param {string} code
+ * @returns {*}
+ */
 export function evaluateExpression(functionality: Functionality, code: string): any {
     const result = evaluateExpressionWithType(functionality, code);
     return result ? result.value : null;
 }
+
+
+/**
+ * Like @see{evaluateExpression}, but returns, type, value tuple.
+ *
+ * @export
+ * @param {Functionality} functionality
+ * @param {string} code
+ * @returns {(IEvalResult | null)}
+ */
 export function evaluateExpressionWithType(functionality: Functionality, code: string): IEvalResult | null {
     const tokens = Lex(code);
     const syntaxTree = tryParseExpression(0, tokens);
